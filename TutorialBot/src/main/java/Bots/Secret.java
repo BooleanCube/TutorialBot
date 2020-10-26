368cb95edee46789299f59b3ce63c73547c59e1f
BooleanCubeYT <47650058+BooleanCube@users.noreply.github.com>
2020-10-26 19:20:22 -0400

    Update Secret.java

--- TutorialBot/src/main/java/Bots/Secret.java
+++ TutorialBot/src/main/java/Bots/Secret.java
@@ -1,6 +1,6 @@
 package Bots;
 
 public class Secret {
-    public static String TutorialBotToken = "NzQwNjIzNTg0NTAxODI1NTU3.Xyrtdg.dj6SX60O039s67DYNU8rbSyrTvw";
+    public static String TutorialBotToken = "TOKEN";
 }
 
